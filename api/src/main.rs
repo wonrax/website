7315f3825967c6d482ab8eda0e937cd553240369
wonrax <hahuylonghai2012@gmail.com>
2023-08-12 17:06:52 +0700

    render html template

--- api/src/main.rs
+++ api/src/main.rs
@@ -5,12 +5,14 @@
 //! ```
 
 use axum::{response::Html, routing::get, Router};
+use handlebars::Handlebars;
 use std::net::SocketAddr;
+use serde_json::json;
 
 #[tokio::main]
 async fn main() {
     // build our application with a route
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new().route("/github", get(handler));
 
     // run it
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
@@ -21,6 +23,10 @@ async fn main() {
         .unwrap();
 }
 
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
+const TEMPLATE_HTML: &'static str = include_str!("github.html");
+
+async fn handler() -> Html<String> {
+    let reg = Handlebars::new();
+    let res = reg.render_template(TEMPLATE_HTML, &json!({"views": 190})).unwrap();
+    Html(res)
 }
