6ed96de6f0752d2c86df7ed0d333f16d9d32fee2
wonrax <hahuylonghai2012@gmail.com>
2023-10-03 17:38:07 +0700

    refactor: remove nextjs

--- apps/hhai.dev/app/blog/[slug]/constants.ts
+++
@@ -1,9 +0,0 @@
-// These will be used for:
-// - Set the layout width of each part of the blog post
-// - Determine reponsive images' srcSizes
-// If you changes these, you also have to change:
-// - feature-sm and feature-lg classes in ./styles.css
-// - Grid layout col width
-export const BLOG_LINE_LENGTH = 700;
-export const BLOG_FEATURE_SM_MAX_LENGTH = 900;
-export const BLOG_FEATURE_LG_MAX_LENGTH = 1920;
