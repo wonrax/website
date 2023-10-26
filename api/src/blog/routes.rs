9658e11dec648c437ae323fcf6f86e6f90d77838
wonrax <hahuylonghai2012@gmail.com>
2023-10-26 18:42:10 +0700

    feat(blog-comments): i think it's not working

--- api/src/blog/routes.rs
+++ api/src/blog/routes.rs
@@ -1,3 +1,8 @@
+use std::{
+    collections::HashMap,
+    rc::Rc,
+};
+
 use axum::{
     extract::{Path, State},
     http::StatusCode,
@@ -21,7 +26,7 @@ struct ErrorResponse {
     msg: Option<String>,
 }
 
-#[derive(FromRow, Debug, Serialize)]
+#[derive(FromRow, Debug, Serialize, Clone)]
 struct Comment {
     id: i32,
     author_ip: String,
@@ -33,12 +38,22 @@ struct Comment {
     created_at: chrono::NaiveDateTime,
 }
 
+#[derive(Debug, Clone, Serialize)]
+struct CommentIntermediate {
+    id: i32,
+    author_name: String,
+    content: String,
+    parent_id: Option<i32>,
+    created_at: chrono::NaiveDateTime,
+    children: Option<Vec<Rc<CommentIntermediate>>>,
+}
+
 #[derive(Debug, Serialize)]
 struct CommentView {
     author_name: String,
     content: String,
     created_at: String,
-    children: Vec<CommentView>,
+    children: Option<Vec<CommentView>>,
 }
 
 enum AppError {
@@ -53,15 +68,15 @@ impl IntoResponse for AppError {
                 ErrorResponse {
                     code: "DB_ERR".into(),
                     msg: Some(format!("Fetching data error: {}", e.to_string())),
-                })
+                },
+            ),
         };
 
         (status_code, Json(json!(error_response))).into_response()
     }
 }
 
-impl From<sqlx::Error> for AppError
-{
+impl From<sqlx::Error> for AppError {
     fn from(e: sqlx::Error) -> Self {
         AppError::DatabaseError(e)
     }
@@ -70,10 +85,10 @@ impl From<sqlx::Error> for AppError
 async fn get_blog_post_comments(
     State(ctx): State<APIContext>,
     Path(slug): Path<String>,
-) -> Result<Json<Vec<Comment>>, AppError> {
+) -> Result<Json<Vec<Rc<CommentIntermediate>>>, AppError> {
     let rows = sqlx::query_as::<_, Comment>(
         "
-        SELECT * FROM comments
+        SELECT comments.* FROM comments
         JOIN posts ON posts.id = comments.post_id
         WHERE posts.slug = $1;
         ",
@@ -82,5 +97,50 @@ async fn get_blog_post_comments(
     .fetch_all(&ctx.pool)
     .await?;
 
-    Ok(Json(rows))
+    let nested = turn_flat_comments_to_nested(rows.clone());
+
+    Ok(Json(nested))
+}
+
+fn turn_flat_comments_to_nested(mut comments: Vec<Comment>) -> Vec<Rc<CommentIntermediate>> {
+    comments.sort_by(|a, b| a.id.cmp(&b.id));
+
+    let mut tree = HashMap::<i32, Rc<CommentIntermediate>>::new();
+    for comment in comments {
+        let c = Rc::new(CommentIntermediate {
+            id: comment.id,
+            author_name: comment.author_name,
+            content: comment.content,
+            parent_id: comment.parent_id,
+            created_at: comment.created_at,
+            children: None,
+        });
+
+        tree.insert(c.id, c.clone());
+        println!("cid {:?}", c.id);
+        if let Some(parent_id) = c.parent_id {
+            let parent = tree.get_mut(&parent_id);
+            if let Some(parent) = parent {
+                let mut_parent = Rc::make_mut(parent);
+                if let Some(children) = mut_parent.children.as_mut() {
+                    children.push(c.clone());
+                } else {
+                    let children = vec![c.clone()];
+                    mut_parent.children = Some(children);
+                }
+                println!("children of {:?}: {:?}", parent_id, mut_parent.children);
+            }
+        }
+    }
+
+    let mut final_comments: Vec<Rc<CommentIntermediate>> = vec![];
+    for (_, comment) in &tree {
+        if comment.parent_id.is_none() {
+            final_comments.push(comment.clone());
+        }
+    }
+
+    println!("tree {:?}", tree);
+
+    final_comments
 }
