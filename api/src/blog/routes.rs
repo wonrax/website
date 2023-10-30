13439455f49e090d71e890f0b3e4a63961512f54
wonrax <hahuylonghai2012@gmail.com>
2023-10-30 17:45:03 +0700

    feat(blog-comments): sort by upvote and date created

--- api/src/blog/routes.rs
+++ api/src/blog/routes.rs
@@ -1,16 +1,13 @@
-use std::{
-    collections::HashMap,
-    rc::Rc,
-};
+use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
 use axum::{
-    extract::{Path, State, Query},
+    extract::{Path, Query, State},
     http::StatusCode,
     response::IntoResponse,
     routing::get,
     Json, Router,
 };
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use serde_json::json;
 use sqlx::FromRow;
 
@@ -29,31 +26,22 @@ struct ErrorResponse {
 #[derive(FromRow, Debug, Serialize, Clone)]
 struct Comment {
     id: i32,
-    author_ip: String,
     author_name: String,
-    author_email: Option<String>,
     content: String,
-    post_id: i32,
     parent_id: Option<i32>,
     created_at: chrono::NaiveDateTime,
+    upvote: i32,
 }
 
 #[derive(Debug, Clone, Serialize)]
-struct CommentIntermediate {
+struct CommentView {
     id: i32,
     author_name: String,
     content: String,
     parent_id: Option<i32>,
     created_at: chrono::NaiveDateTime,
-    children: Option<Vec<Rc<CommentIntermediate>>>,
-}
-
-#[derive(Debug, Serialize)]
-struct CommentView {
-    author_name: String,
-    content: String,
-    created_at: String,
-    children: Option<Vec<CommentView>>,
+    children: Option<Vec<Rc<RefCell<CommentView>>>>,
+    upvote: i32,
 }
 
 enum AppError {
@@ -91,8 +79,8 @@ struct Pagination {
 async fn get_blog_post_comments(
     State(ctx): State<APIContext>,
     Path(slug): Path<String>,
-    pagination: Query<Pagination>
-) -> Result<Json<Vec<Rc<CommentIntermediate>>>, AppError> {
+    pagination: Query<Pagination>,
+) -> Result<Json<Vec<Rc<RefCell<CommentView>>>>, AppError> {
     let rows = sqlx::query_as::<_, Comment>(
         "
         WITH RECURSIVE root_comments AS (
@@ -108,10 +96,10 @@ async fn get_blog_post_comments(
             FROM comments
             JOIN posts ON (posts.id = comments.post_id)
             WHERE posts.category = 'blog'
-            AND posts.slug = 'authoring-in-markdown'
+            AND posts.slug = $1
             AND comments.parent_id IS NULL
             ORDER BY comments.upvote DESC, comments.created_at
-            LIMIT 10 OFFSET 0
+            LIMIT $2 OFFSET $3
         ), t(parent_id, id, author_name, content, root, level, created_at, upvote) AS (
             (
                 SELECT * FROM root_comments
@@ -133,55 +121,86 @@ async fn get_blog_post_comments(
         ORDER BY root;
         ",
     )
+    .bind(slug)
     .bind(pagination.page_size as i64)
     .bind(pagination.page_offset as i64)
     .fetch_all(&ctx.pool)
     .await?;
 
-    let nested = turn_flat_comments_to_nested(rows.clone());
+    let mut nested = turn_flat_comments_to_nested(rows.clone());
+    sort_comments_by_upvote(&mut nested);
 
-    Ok(Json(nested))
+    let mut result: Vec<Rc<RefCell<CommentView>>> = vec![];
+    for comment in nested {
+        depth_first_search(comment.clone(), &mut result);
+    }
+
+    // remove all children that are still referenced
+    for comment in &result {
+        comment.borrow_mut().children = None;
+    }
+
+    Ok(Json(result))
 }
 
-fn turn_flat_comments_to_nested(mut comments: Vec<Comment>) -> Vec<Rc<CommentIntermediate>> {
-    comments.sort_by(|a, b| a.id.cmp(&b.id));
-
-    let mut tree = HashMap::<i32, Rc<CommentIntermediate>>::new();
+fn turn_flat_comments_to_nested(comments: Vec<Comment>) -> Vec<Rc<RefCell<CommentView>>> {
+    let mut tree = HashMap::<i32, Rc<RefCell<CommentView>>>::new();
     for comment in comments {
-        let c = Rc::new(CommentIntermediate {
+        let c = Rc::new(RefCell::new(CommentView {
             id: comment.id,
             author_name: comment.author_name,
             content: comment.content,
             parent_id: comment.parent_id,
             created_at: comment.created_at,
             children: None,
-        });
+            upvote: comment.upvote,
+        }));
 
-        tree.insert(c.id, c.clone());
-        println!("cid {:?}", c.id);
-        if let Some(parent_id) = c.parent_id {
-            let parent = tree.get_mut(&parent_id);
+        tree.insert(c.borrow().id, c.clone());
+        if let Some(parent_id) = c.borrow().parent_id {
+            let parent = tree.get(&parent_id);
             if let Some(parent) = parent {
-                let mut_parent = Rc::make_mut(parent);
+                let mut mut_parent = parent.borrow_mut();
                 if let Some(children) = mut_parent.children.as_mut() {
                     children.push(c.clone());
                 } else {
                     let children = vec![c.clone()];
                     mut_parent.children = Some(children);
                 }
-                println!("children of {:?}: {:?}", parent_id, mut_parent.children);
             }
-        }
+        };
     }
 
-    let mut final_comments: Vec<Rc<CommentIntermediate>> = vec![];
+    let mut final_comments: Vec<Rc<RefCell<CommentView>>> = vec![];
     for (_, comment) in &tree {
-        if comment.parent_id.is_none() {
+        if comment.borrow().parent_id.is_none() {
             final_comments.push(comment.clone());
         }
     }
 
-    println!("tree {:?}", tree);
-
     final_comments
+}
+
+fn sort_comments_by_upvote(comments: &mut Vec<Rc<RefCell<CommentView>>>) {
+    // sort the array
+    comments.sort_unstable_by_key(|k| (-k.borrow().upvote, k.borrow().created_at));
+
+    // sort the children
+    for comment in comments {
+        if let Some(children) = comment.borrow_mut().children.as_mut() {
+            sort_comments_by_upvote(children);
+        }
+    }
+}
+
+fn depth_first_search(
+    comment: Rc<RefCell<CommentView>>,
+    mut result: &mut Vec<Rc<RefCell<CommentView>>>,
+) {
+    result.push(comment.clone());
+    if let Some(children) = comment.borrow().children.as_ref() {
+        for child in children {
+            depth_first_search(child.clone(), &mut result);
+        }
+    }
 }
