4867916b5827c2f52ebabc03149905ee2534311c
wonrax <hahuylonghai2012@gmail.com>
2023-12-25 16:42:44 +0700

    feat(og-image): add og image for all routes

---
+++ apps/hhai.dev/src/components/OgImage/generate.ts
@@ -0,0 +1,58 @@
+import satori from "satori";
+import OgImage from "./OgImageGenerator";
+import { Resvg } from "@resvg/resvg-js";
+
+type Props = {
+  title: string;
+  description?: string;
+};
+
+const interNormal = await (
+  await fetch(
+    "https://cdn.jsdelivr.net/npm/@fontsource/inter@5.0.16/files/inter-latin-400-normal.woff"
+  )
+).arrayBuffer();
+
+const interBold = await (
+  await fetch(
+    "https://cdn.jsdelivr.net/npm/@fontsource/inter@5.0.16/files/inter-latin-700-normal.woff"
+  )
+).arrayBuffer();
+
+export default async function getOgImageResponse(props: Props) {
+  // TODO make a cache out of this for efficient build and bandwith
+  const svg = await satori(
+    await OgImage({
+      title: props.title,
+      description: props.description,
+    }),
+    {
+      width: 1200,
+      height: 630,
+      fonts: [
+        {
+          name: "Inter",
+          data: interNormal,
+          weight: 400,
+          style: "normal",
+        },
+        {
+          name: "Inter",
+          data: interBold,
+          weight: 700,
+          style: "normal",
+        },
+      ],
+    }
+  );
+
+  const resvg = new Resvg(svg);
+  const pngData = resvg.render();
+  const pngBuffer = pngData.asPng();
+
+  return new Response(pngBuffer, {
+    headers: {
+      "Content-Type": "image/png",
+    },
+  });
+}
