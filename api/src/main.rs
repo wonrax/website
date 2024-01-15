8d209ced2edd1adf7f4d528759ddf29ac0b39edc
wonrax <hahuylonghai2012@gmail.com>
2024-01-15 11:44:42 +0700

    feat(blog-comments): add universal client ip extension

--- api/src/main.rs
+++ api/src/main.rs
@@ -67,7 +67,12 @@ async fn main() {
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("listening on {}", "0.0.0.0:3000");
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(
+        listener,
+        app.into_make_service_with_connect_info::<SocketAddr>(),
+    )
+    .await
+    .unwrap();
 }
 
 #[derive(Serialize)]
