db7ffdf77ef70968dc31840404a95baeda3920cf
wonrax <hahuylonghai2012@gmail.com>
2024-03-18 10:35:28 +0700

    feat(www): handle errors in the GitHub callback page

---
+++ apps/hhai.dev/src/rpc.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const ApiError = z.object({
+  code: z.optional(z.string()),
+  msg: z.string(),
+  reason: z.optional(z.string()),
+  // debug_info: z.optional(z.string()),
+});
