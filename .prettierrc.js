517a4074f9f9ef6cce5343f67791acad346e852d
wonrax <hahuylonghai2012@gmail.com>
2024-05-25 14:00:44 +0700

    chore: add prettier js config

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  plugins: ['prettier-plugin-astro'],
+};
+
+module.exports = config;
+
