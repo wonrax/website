7abfe2fa623313297b262b055e94789ae8962be1
wonrax <hahuylonghai2012@gmail.com>
2025-05-13 15:13:26 +0700

    feat(web): use SITE_URL env var for API endpoint

--- apps/hhai.dev/src/config.ts
+++ apps/hhai.dev/src/config.ts
@@ -1,9 +1,9 @@
 const config = {
   API_URL:
-    (import.meta.env.PUBLIC_API_URL as string | undefined) ??
-    (import.meta.env.PROD
-      ? ("https://hhai.dev/api" as const)
-      : ("http://localhost:3000" as const)),
+    ((import.meta.env.SITE_URL as string | undefined) ??
+      (import.meta.env.PROD
+        ? ("https://hhai.dev" as const)
+        : ("http://localhost:3000" as const))) + "/api",
 };
 
 export default config;
--- docker/hhai-dev-www.Dockerfile
+++ docker/hhai-dev-www.Dockerfile
@@ -15,6 +15,7 @@ COPY turbo.json .
 
 # Build 'hhai.dev's dependencies, but not 'hhai.dev' itself
 # This enable caching for the dependencies build layer
+ENV SITE_URL=https://wrx.sh
 RUN npx turbo build --filter=hhai.dev^...
 
 COPY apps/. ./apps
